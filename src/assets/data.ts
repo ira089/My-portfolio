43c85be0835fb1b95d09982f5fd1d6cf2680087c
ira089 <gvir2015@gmail.com>
2025-01-23 12:56:45 +0200

    add img petLove

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,15 +1,16 @@
 import { nanoid } from "nanoid";
 import { IProject } from "../models";
-import LL1 from "./image/Learn Lingo1.jpg";
-// import LL from "./image/Learn Lingo .jpg";
-import LL3 from "./image/Learn Lingo3.jpg";
-import LL2 from "./image/Learn Lingo2.png";
 
 const projects: IProject[] = [
   {
     title: "E-Pharmacy",
     id: nanoid(),
-    photos: ["https://i.imgur.com/bwLHrNs.png"],
+    photos: [
+      "https://i.imgur.com/mWv2xlN.jpg",
+      "https://i.imgur.com/JB9EOip.jpg",
+      "https://i.imgur.com/2PbSPbE.jpg",
+      "https://i.imgur.com/lRAQM0z.jpg",
+    ],
     gitUrl: "https://github.com/ira089/Pharmacy",
     website: "https://ira089.github.io/Pharmacy/",
     skills: [
@@ -21,7 +22,6 @@ const projects: IProject[] = [
       "material UI",
       "Redux",
       "axios",
-
       "Webpack",
     ],
     description: "E-Pharmacy",
@@ -29,20 +29,41 @@ const projects: IProject[] = [
   {
     title: "LearnForeignLanguage",
     id: nanoid(),
-    photos: ["https://i.imgur.com/BA46ORF.png", LL1, LL2, LL3],
+    photos: [
+      "https://i.imgur.com/0YjX0Se.jpg",
+      "https://i.imgur.com/5PLHwMS.jpg",
+      "https://i.imgur.com/CzMz0tN.jpg",
+      "https://i.imgur.com/dWgN5Lq.png",
+    ],
     gitUrl: "https://github.com/ira089/LearnForeignLanguage",
     website: "https://ira089.github.io/LearnForeignLanguage/",
     skills: ["JavaScript", "React", "HTML", "CSS", "Redux", "Webpack"],
     description: "LearnForeignLanguage",
   },
   {
-    title: "E-Pharmacy",
+    title: "Pet Love",
     id: nanoid(),
-    photos: ["https://i.imgur.com/bwLHrNs.png"],
-    gitUrl: "https://github.com/ira089/Pharmacy",
-    website: "https://ira089.github.io/Pharmacy/",
-    skills: ["JavaScript", "material UI", "Redux", "axios"],
-    description: "E-Pharmacy upd",
+    photos: [
+      "https://i.imgur.com/omWftUv.jpg",
+      "https://i.imgur.com/CNSVtCK.jpg",
+      "https://i.imgur.com/LBzWQUd.jpg",
+      "https://i.imgur.com/EwtbYCx.jpg",
+    ],
+    gitUrl: "https://github.com/ira089/PetLove",
+    website: "https://ira089.github.io/PetLove/",
+    skills: [
+      "JavaScript",
+      "React",
+      "HTML",
+      "CSS",
+      "REST API",
+      "material UI",
+      "Redux",
+      "axios",
+      "Webpack",
+    ],
+    description:
+      "The project was created according to the layout Figma. Additional technologies used: react-hook-form, react-router-dom, date-fns, formik, yup, react-select, react-toastify, redux-persist. This is an app about pets. You can record your pet's details, read about other people's pets, sort animals by popularity, price, location, animal type, or category. You can search simply by keyword. You can read news about animals or see partners' contacts. ",
   },
   {
     title: "E-Pharmacy",
